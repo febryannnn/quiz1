4e1971bd4784cd76df93174c21015cf347ac4ce2
febryannnn <farelfebryan06@gmail.com>
2025-09-23 03:42:05 +0700

    update

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ const route = (event) => {
   console.log(path)
   handleLocation();
 };
+const style_css = document.getElementById("page-style");
+
 
 const routes = {
-  "/quiz1": "/quiz1/pages-client/index.html",
-  "/quiz1/profile": "/quiz1/pages-client/profile-client.html",
-  "/quiz1/hometown": "/quiz1/pages-client/hometown-client.html",
-  "/quiz1/food": "/quiz1/pages-client/food-client.html",
-  "/quiz1/tourist": "/quiz1/pages-client/tourist-client.html",
+  "/quiz1": { html: "/quiz1/pages-client/index.html", css: "/quiz1/css/home.css" },
+  "/quiz1/profile": { html: "/quiz1/pages-client/profile-client.html", css: "/quiz1/css/profile.css" },
+  "/quiz1/hometown": { html: "/quiz1/pages-client/hometown-client.html", css: "/quiz1/css/hometown.css" },
+  "/quiz1/food": { html: "/quiz1/pages-client/food-client.html", css: "/quiz1/css/food.css" },
+  "/quiz1/tourist": { html: "/quiz1/pages-client/tourist-client.html", css: "/quiz1/css/tourist.css" }
 };
+
 
 const handleLocation = async () => {
   const path = window.location.pathname;
@@ -22,6 +25,10 @@ const handleLocation = async () => {
   const route = routes[path] || routes["/quiz1"];
   const html = await fetch(route).then((data) => data.text());
   document.getElementById("main-page").innerHTML = html;
+
+  if (route.css) {
+    style_css.setAttribute("href", route.css);
+  }
 };
 
 window.onpopstate = handleLocation;
