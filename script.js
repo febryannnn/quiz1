9811b4a6257ca41bf830dd513221e14035e8e6a2
febryannnn <farelfebryan06@gmail.com>
2025-09-23 03:18:26 +0700

    update

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ const route = (event) => {
   event.preventDefault();
   const path = event.target.getAttribute("href");
   window.history.pushState({}, "", path);
+  console.log(path)
   handleLocation();
 };
 
 const routes = {
-  "/quiz1": "/quiz1-web/pages-client/index.html",
-  "/quiz1/profile": "/quiz1-web/pages-client/profile-client.html",
-  "/quiz1/hometown": "/quiz1-web/pages-client/hometown-client.html",
-  "/quiz1/food": "/quiz1-web/pages-client/food-client.html",
-  "/quiz1/tourist": "/quiz1-web/pages-client/tourist-client.html",
+  "/quiz1": "/pages-client/index.html",
+  "/quiz1/profile": "/pages-client/profile-client.html",
+  "/quiz1/hometown": "/pages-client/hometown-client.html",
+  "/quiz1/food": "/pages-client/food-client.html",
+  "/quiz1/tourist": "/pages-client/tourist-client.html",
 };
 
 const handleLocation = async () => {
   const path = window.location.pathname;
+  console.log(path)
   const route = routes[path] || routes["/quiz1"];
   const html = await fetch(route).then((data) => data.text());
   document.getElementById("main-page").innerHTML = html;
